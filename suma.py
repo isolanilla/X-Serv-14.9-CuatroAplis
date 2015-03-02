b5ee6e0cec1cdef5230f9d5c3644c2b49c97146f
isolanilla <i.solanilla3@gmail.com>
2015-03-02 19:29:27 +0100

    ejercicio_con_pep8

--- suma.py
+++ suma.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python
 
 
+operaciones = ["suma", "resta"]
 
-operaciones = ["suma","resta"]
+
 class suma:
-    def parse(self,peticion,rest):
+
+    def parse(self, peticion, rest):
 
         try:
-            paquete = peticion.split()[1][1:] 
-            lista = paquete.split('/') # lista =(operacion, operando1, operando2)
+            paquete = peticion.split()[1][1:]
+            lista = paquete.split('/')
         except ValueError:
             return None
         if len(lista) != 3 or lista[0] not in operaciones:
             return None
 
-        return lista 
+        return lista
 
-    def process(self,parsedRequest):
-       
+    def process(self, parsedRequest):
+
         if not parsedRequest:
-            return ("400 Bad Request","GO AWAY")
+            return ("400 Bad Request", "GO AWAY")
 
         (operacion, operando1, operando2) = parsedRequest
 
@@ -28,5 +30,4 @@ class suma:
         elif operacion == "resta":
             resultado = int(operando1) - int(operando2)
 
-        return ("200 OK","<html><body><h1>" + str(resultado) + "</body></html>")
-
+        return ("200 OK", "<html><body><h1>" + str(resultado) + "</body></html>")
