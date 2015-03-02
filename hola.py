0d77393fceb8f1f103062d833244b6afcfbb4970
isolanilla <i.solanilla3@gmail.com>
2015-03-02 19:19:34 +0100

    ejercicio_terminado

---
+++ hola.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+
+import webapp
+import aleat
+import suma
+
+class hola():
+
+    def parse(self, request, rest):
+        return None
+
+
+    def process(self,parsedRequest):
+
+        return ("200 OK","<html><body><h1>HOLA</body></html>")
+
+
+
+class adios():
+    def parse(self, request, rest):
+        return None
+
+    def process(self,parsedRequest):
+
+        return ("200 OK","<html><body><h1>ADIOS</body></html>")
+
+
+if __name__ == "__main__":
+    hola = hola()
+    adios = adios()
+    aleat = aleat.aleat()
+    suma = suma.suma()
+    classapps = {"/hola": hola,"/resta":suma ,"/adios": adios, "/aleat": aleat, "/suma": suma}
+    try:
+        testWebApp = webapp.webApp("localhost", 1234, classapps)
+    except KeyboardInterrupt:
+        print "Key board interrupt"
+
