9c30b678bdfe9f61e659e36a27c04f5746c950a8
isolanilla <i.solanilla3@gmail.com>
2015-03-02 19:22:19 +0100

    ejercicio_ejecutable

---
+++ suma.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+
+
+operaciones = ["suma","resta"]
+class suma:
+    def parse(self,peticion,rest):
+
+        try:
+            paquete = peticion.split()[1][1:] 
+            lista = paquete.split('/') # lista =(operacion, operando1, operando2)
+        except ValueError:
+            return None
+        if len(lista) != 3 or lista[0] not in operaciones:
+            return None
+
+        return lista 
+
+    def process(self,parsedRequest):
+       
+        if not parsedRequest:
+            return ("400 Bad Request","GO AWAY")
+
+        (operacion, operando1, operando2) = parsedRequest
+
+        if operacion == "suma":
+            resultado = int(operando1) + int(operando2)
+        elif operacion == "resta":
+            resultado = int(operando1) - int(operando2)
+
+        return ("200 OK","<html><body><h1>" + str(resultado) + "</body></html>")
+
